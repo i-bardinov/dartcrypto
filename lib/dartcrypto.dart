f865af71d7903bf4e17e4992bc7b1ce9d363993a
ilya-bardinov <ilya.bardinov@gmail.com>
2015-10-28 14:05:12 +0200

    change structure

--- lib/dartcrypto.dart
+++ lib/dartcrypto.dart
@@ -1,13 +1,13 @@
 
 library dartcrypto;
 
-export 'package:dartcrypto/src/block_ciphers/aes.dart';
-export 'package:dartcrypto/src/block_ciphers/affine.dart';
-export 'package:dartcrypto/src/block_ciphers/beaufort.dart';
-export 'package:dartcrypto/src/block_ciphers/hill.dart';
-export 'package:dartcrypto/src/block_ciphers/vigenere.dart';
-export 'package:dartcrypto/src/block_ciphers/vernam.dart';
-export 'package:dartcrypto/src/block_ciphers/gost_28147_89.dart';
+export 'package:dartcrypto/src/symmetric_key/aes.dart';
+export 'package:dartcrypto/src/symmetric_key/affine.dart';
+export 'package:dartcrypto/src/symmetric_key/beaufort.dart';
+export 'package:dartcrypto/src/symmetric_key/hill.dart';
+export 'package:dartcrypto/src/symmetric_key/vigenere.dart';
+export 'package:dartcrypto/src/symmetric_key/vernam.dart';
+export 'package:dartcrypto/src/symmetric_key/gost_28147_89.dart';
 
 export 'package:dartcrypto/src/public_key/rsa.dart';
 
