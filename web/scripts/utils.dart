781856c873921a16d458555b656cefca6f4b1b28
ilya-bardinov <ilya.bardinov@gmail.com>
2015-11-01 20:48:06 +0200

    implement smooth scrolling and descriptions of ciphers

--- web/scripts/utils.dart
+++ web/scripts/utils.dart
@@ -17,12 +17,14 @@ const String BUTTON_GENERATE_KEY = "Generate Key";
 const String BUTTON_ENCRYPT_MESSAGE = "Encrypt";
 const String BUTTON_DECRYPT_MESSAGE = "Decrypt";
 
-const String TEXT_DESCRIPTION_CAESAR = "";
+const String TEXT_DESCRIPTION_CAESAR =
+    "In cryptography, a <a href=\"https://en.wikipedia.org/wiki/Caesar_cipher\" target=\"_blank\">Caesar cipher</a>, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.";
 const String TEXT_DESCRIPTION_AFFINE = "";
 const String TEXT_DESCRIPTION_HILL = "";
 const String TEXT_DESCRIPTION_VIGENERE = "";
 const String TEXT_DESCRIPTION_BEAUFORT = "";
 const String TEXT_DESCRIPTION_VERNAM = "";
+const String TEXT_DESCRIPTION_AES = "";
 const String TEXT_DESCRIPTION_MAGMA = "";
 
 const int CIPHER_CAESAR = 0;
